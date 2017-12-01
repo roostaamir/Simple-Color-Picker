365a8a8c8a9689b212412be18a12f8debbb06a40
roostaamir <roostaamir73@gmail.com>
2017-12-02 00:17:17 +0100

    Initial Commit

---
+++ ColorPicker/ColorPicker/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ColorPicker
+{
+    partial class MainFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrom));
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRed = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGreen = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBlue = new System.Windows.Forms.PictureBox();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            this.Blue = new System.Windows.Forms.Label();
+            this.lblRedValue = new System.Windows.Forms.Label();
+            this.lblGreenValue = new System.Windows.Forms.Label();
+            this.lblBlueValue = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.lblHexValue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 32);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(336, 50);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // pictureBoxRed
+            // 
+            this.pictureBoxRed.Location = new System.Drawing.Point(12, 124);
+            this.pictureBoxRed.Name = "pictureBoxRed";
+            this.pictureBoxRed.Size = new System.Drawing.Size(97, 50);
+            this.pictureBoxRed.TabIndex = 5;
+            this.pictureBoxRed.TabStop = false;
+            // 
+            // pictureBoxGreen
+            // 
+            this.pictureBoxGreen.Location = new System.Drawing.Point(133, 124);
+            this.pictureBoxGreen.Name = "pictureBoxGreen";
+            this.pictureBoxGreen.Size = new System.Drawing.Size(97, 50);
+            this.pictureBoxGreen.TabIndex = 6;
+            this.pictureBoxGreen.TabStop = false;
+            // 
+            // pictureBoxBlue
+            // 
+            this.pictureBoxBlue.Location = new System.Drawing.Point(251, 124);
+            this.pictureBoxBlue.Name = "pictureBoxBlue";
+            this.pictureBoxBlue.Size = new System.Drawing.Size(97, 50);
+            this.pictureBoxBlue.TabIndex = 7;
+            this.pictureBoxBlue.TabStop = false;
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.Location = new System.Drawing.Point(12, 101);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(46, 17);
+            this.lblRed.TabIndex = 8;
+            this.lblRed.Text = "Red : ";
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.AutoSize = true;
+            this.lblGreen.Location = new System.Drawing.Point(130, 100);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(60, 17);
+            this.lblGreen.TabIndex = 9;
+            this.lblGreen.Text = "Green : ";
+            // 
+            // Blue
+            // 
+            this.Blue.AutoSize = true;
+            this.Blue.Location = new System.Drawing.Point(248, 100);
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(48, 17);
+            this.Blue.TabIndex = 10;
+            this.Blue.Text = "Blue : ";
+            // 
+            // lblRedValue
+            // 
+            this.lblRedValue.AutoSize = true;
+            this.lblRedValue.Location = new System.Drawing.Point(63, 101);
+            this.lblRedValue.Name = "lblRedValue";
+            this.lblRedValue.Size = new System.Drawing.Size(0, 17);
+            this.lblRedValue.TabIndex = 11;
+            // 
+            // lblGreenValue
+            // 
+            this.lblGreenValue.AutoSize = true;
+            this.lblGreenValue.Location = new System.Drawing.Point(184, 101);
+            this.lblGreenValue.Name = "lblGreenValue";
+            this.lblGreenValue.Size = new System.Drawing.Size(0, 17);
+            this.lblGreenValue.TabIndex = 12;
+            // 
+            // lblBlueValue
+            // 
+            this.lblBlueValue.AutoSize = true;
+            this.lblBlueValue.Location = new System.Drawing.Point(302, 100);
+            this.lblBlueValue.Name = "lblBlueValue";
+            this.lblBlueValue.Size = new System.Drawing.Size(0, 17);
+            this.lblBlueValue.TabIndex = 13;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(12, 208);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(84, 17);
+            this.label.TabIndex = 14;
+            this.label.Text = "Hex Value : ";
+            // 
+            // lblHexValue
+            // 
+            this.lblHexValue.AutoSize = true;
+            this.lblHexValue.Location = new System.Drawing.Point(103, 208);
+            this.lblHexValue.Name = "lblHexValue";
+            this.lblHexValue.Size = new System.Drawing.Size(0, 17);
+            this.lblHexValue.TabIndex = 15;
+            // 
+            // MainFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 253);
+            this.Controls.Add(this.lblHexValue);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.lblBlueValue);
+            this.Controls.Add(this.lblGreenValue);
+            this.Controls.Add(this.lblRedValue);
+            this.Controls.Add(this.Blue);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.pictureBoxBlue);
+            this.Controls.Add(this.pictureBoxGreen);
+            this.Controls.Add(this.pictureBoxRed);
+            this.Controls.Add(this.pictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainFrom";
+            this.Text = "Color Picker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFrom_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.PictureBox pictureBoxRed;
+        private System.Windows.Forms.PictureBox pictureBoxGreen;
+        private System.Windows.Forms.PictureBox pictureBoxBlue;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblGreen;
+        private System.Windows.Forms.Label Blue;
+        private System.Windows.Forms.Label lblRedValue;
+        private System.Windows.Forms.Label lblGreenValue;
+        private System.Windows.Forms.Label lblBlueValue;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label lblHexValue;
+    }
+}
+
---
+++ ColorPicker/ColorPicker/Form1.cs
@@ -0,0 +1,106 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorPicker
+{
+    public partial class MainFrom : Form
+    {
+        private IKeyboardMouseEvents _keyboardMouseEvents;
+        private bool _isControlPressed = false;
+        private Color _pixelColor;
+        private Color _redOnly;
+        private Color _blueOnly;
+        private Color _greenOnly;
+
+        public MainFrom()
+        {
+            InitializeComponent();
+            SubscribeMouseEvents(Hook.GlobalEvents());
+        }
+
+        private void SubscribeMouseEvents(IKeyboardMouseEvents events)
+        {
+            _keyboardMouseEvents = events;
+            _keyboardMouseEvents.MouseMove += OnMouseMove;
+            _keyboardMouseEvents.KeyDown += OnKeyDown;
+            _keyboardMouseEvents.KeyUp += OnKeyUp;
+        }
+
+        private void UnSubscribeMouseEvents()
+        {
+            if (_keyboardMouseEvents == null) return;
+            _keyboardMouseEvents.MouseMove -= OnMouseMove; 
+            _keyboardMouseEvents.KeyDown -= OnKeyDown;
+            _keyboardMouseEvents.KeyUp -= OnKeyUp;
+            _keyboardMouseEvents.Dispose();
+            _keyboardMouseEvents = null;
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            _pixelColor = ColorPicker.GetPixelColor(e.X, e.Y);
+            pictureBox.BackColor = _pixelColor;
+            pictureBoxRed.BackColor = Color.FromArgb(_pixelColor.R, 0, 0);
+            pictureBoxGreen.BackColor = Color.FromArgb(0, _pixelColor.G, 0);
+            pictureBoxBlue.BackColor = Color.FromArgb(0, 0, _pixelColor.B);
+
+            lblRedValue.Text = _pixelColor.R.ToString();
+            lblGreenValue.Text = _pixelColor.G.ToString();
+            lblBlueValue.Text = _pixelColor.B.ToString();
+            lblHexValue.Text = RGBToHex(_pixelColor);
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.LControlKey:
+                case Keys.RControlKey:
+                    _isControlPressed = true;
+                    break;
+                case Keys.G:
+                    if (_isControlPressed) Clipboard.SetText(RGBToHex(_pixelColor));
+                    break;
+                default:
+                    break;
+            }
+                
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.LControlKey:
+                case Keys.RControlKey:
+                    _isControlPressed = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MainFrom_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnSubscribeMouseEvents();
+        }
+
+        private static String RGBToHex(Color color)
+        {
+            return "#" + color.R.ToString("x2") + color.G.ToString("x2") + color.B.ToString("x2");
+        }
+
+        private static String ARGBToHex(Color color)
+        {
+            return "#" + color.A.ToString("x2") + color.R.ToString("x2") + color.G.ToString("x2") + color.B.ToString("X2");
+        }
+    }
+}
